953db7346a1f04632581026e03e2fa0fed453c56
shariq shah <shariqshah1235@gmail.com>
2021-11-17 21:14:52 +0530

    Create quiz3.java

---
+++ quiz3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class quiz3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+
+        if (button == 1 ){
+            System.out.println("Hello ");
+        }
+        else if ( button == 2 ){
+            System.out.println("Namaste");
+        }
+        else if (button == 3){
+            System.out.println("Bonjour");
+        }
+        else {
+            System.out.println("Invalid button");
+        }
+    }
+}
