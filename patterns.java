d702e903591168390c7ea73691f620d41c2cc6f5
shariq shah <shariqshah1235@gmail.com>
2021-11-17 21:14:43 +0530

    Create patterns.java

---
+++ patterns.java
@@ -0,0 +1,14 @@
+public class patterns {
+    public static void main(String[] args) {
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+//  quiz 
+// *
+// **  output should be his 
+
+System.out.println("*\n**");
+
+ 
+    }
+}
