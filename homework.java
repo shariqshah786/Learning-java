ccf62554717e84f83e8df0d2d107586b9dfe4808
shariq shah <shariqshah1235@gmail.com>
2021-11-17 21:14:25 +0530

    Create homework.java

---
+++ homework.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class homework {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+       
+    double r=sc.nextFloat();
+    double  area=(22*r*r)/7;
+
+    System.out.println("Area of a circle is "+ area);
+
+    }
+}
