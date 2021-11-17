09471d8b3e5fe53e272c619af19ee2eab88739c2
shariq shah <shariqshah1235@gmail.com>
2021-11-17 21:14:38 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // String name = "tony stark ";
+        // int age = 48;
+        // double price = 564.314;
+        // int a = 23;
+        // int b =34;
+
+        // b=23;
+        // name="iron man";
+        // System.out.println(name);
+
+        int a = 23;
+        int b = 34;
+
+        // int sum = a+b;
+        // System.out.println(sum);
+       int diff = a-b;
+       System.out.println(diff);
+
+    }
+
+}
