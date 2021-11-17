9728748bd918c10fa6a7f5291ea5ccb3f2b8a9c2
shariq shah <shariqshah1235@gmail.com>
2021-11-17 21:14:46 +0530

    Create quiz.java

---
+++ quiz.java
@@ -0,0 +1,11 @@
+public class quiz {
+    public static void main(String[] args) {
+        int a =10 ;
+        int b =5 ;
+        // int sum = a*b / a-b;
+    //  System.out.println(sum);     
+   int diff = (a*b)/(a-b);
+   System.out.println(diff);
+
+    }
+}
