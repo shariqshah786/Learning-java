c4fc6aa6a639daa4a3b71eb8aba540c8c80cb16f
shariq shah <shariqshah1235@gmail.com>
2021-11-17 21:14:16 +0530

    Create homework2.java

---
+++ homework2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class homework2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in );
+         System.out.println("Enter Number ");
+        int t = s.nextInt();
+        for(int i= 1;i<=10;i++ ){
+        System.out.println(t+" * "+i+" = "+t*i);
+    }
+    }
+}
