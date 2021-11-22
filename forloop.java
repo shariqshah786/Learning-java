4af2278235fd1d83e57d71300c4fcd8c7f409f2c
shariq shah <shariqshah1235@gmail.com>
2021-11-22 22:28:24 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,22 @@
+
+
+public class forloop {
+    public static void main(String[] args) {
+        // System.out.println("Hello world ");
+        // System.out.println("Hello world ");
+        // System.out.println("Hello world ");
+        // for(int counter =0 ; counter <100; counter = counter+1 ){
+        //     System.out.println("Hello world");
+        // }
+
+     for(int i =1 ; i <11; i ++){
+          System.out.print(i+" ");
+
+     }
+
+
+
+
+
+    }
+}
